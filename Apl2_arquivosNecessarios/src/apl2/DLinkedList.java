20919cf6f44997e6d0febe24e9bc07e3899504d6
Mateus <mateusrcerqueira@gmail.com>
2025-05-07 16:40:09 -0300

    primeiro commit

---
+++ Apl2_arquivosNecessarios/src/apl2/DLinkedList.java
@@ -0,0 +1,177 @@
+// arquivo: src/apl2/DLinkedList.java
+
+// TODO: Colocar a identificação dos(as) integrantes aqui.
+
+package apl2;
+
+// -- A classe DLinkedList (que pertence ao pacote apl2) deve implementar uma
+// lista duplamente encadeada. Os nós dessa lista são do tipo [da classe] Node.
+// -- A classe deve possuir dois nós especiais, head e tail, que são
+// referências para o primeiro e último nó da lista, respectivamente.
+// -- A classe deve possuir um contador de quantos nós existem na lista.
+// -- A classe deve sobrescrever (override) o método public String toString()
+// {...}, retornando uma string com o conteúdo da lista.
+// -- A classe deve implementar as operações a seguir, respeitando o
+// comportamento descrito em cada operação.
+
+public class DLinkedList {
+	
+	private Node head;
+	private Node tail;
+	private int count;
+	
+	public DLinkedList() {
+		head = null;
+		tail = null;
+		count = 0;
+	}
+	
+	public void destroy() {
+		clear();
+	}
+	
+	public void insert(String id, String nome, float nota) {
+		Node node = new Node(id, nome, nota, head);
+		
+		if (isEmpty()) {
+			tail = node;
+		}
+		
+		head = node;
+		++count;
+	}
+	
+	public void append(String id, String nome, float nota) {
+		Node node = new Node(id, nome, nota, null);
+		
+		if (isEmpty()) {
+			head = node;
+		} else {
+			tail.setNext(node);
+		}
+		
+		tail = node;
+		++count;
+	}
+	
+	public Node removeHead() {
+		if (isEmpty()) {
+			return null;
+		}
+		
+		Node toRemove = head;
+
+		head = head.getNext();
+		--count;
+		
+		if (isEmpty()) {
+			tail = null;
+		}
+		
+		toRemove.setNext(null);
+		return toRemove;
+	}
+	
+	public Node removeTail() {
+		if (head == tail)
+			return removeHead();
+		
+		Node toRemove = head;
+		Node previous = null;
+		while (toRemove != tail) {
+			previous = toRemove;
+			toRemove = toRemove.getNext();
+		}
+		
+		tail = previous;
+		tail.setNext(null);
+		--count;
+		
+		toRemove.setNext(null);
+		return toRemove;
+	}
+	
+	public Node removeNode(String id) {
+		Node toRemove = head;
+		Node previous = null;
+		while (toRemove != null && !id.equals(toRemove.getId())) {
+			previous = toRemove;
+			toRemove = toRemove.getNext();
+		}
+		
+		if (toRemove == null) {
+			return null;
+		}
+		
+		if (toRemove == head) {
+			return removeHead();
+		}
+		
+		if (toRemove == tail) {
+			return removeTail();
+		}
+		
+		previous.setNext(toRemove.getNext());
+		--count;
+		
+		toRemove.setNext(null);		
+		return toRemove;
+	}
+
+	public Node getHead() {
+		return head;
+	}
+	
+	public Node getTail() {
+		return tail;
+	}
+	
+	public Node getNode(String id) {
+		Node node = head;
+		while (node != null) {
+			if (node.getId().equals(id)) {
+				return node;
+			}
+			node = node.getNext();
+		}
+		
+		return null;
+	}
+	
+	public int count() {
+		return count;
+	}
+	
+	public boolean isEmpty() {
+		return head == null;
+	}
+	
+	public void clear() {
+		while (!isEmpty()) {
+			removeHead();
+		}
+	}
+
+	@Override
+	public String toString() {
+		StringBuilder sb = new StringBuilder();
+		
+		sb.append("(" + count + ") \n");
+		
+		Node node = head;
+		while (node != null) {
+			sb.append("(")
+			.append(node.getId())
+			.append(";")
+			.append(node.getNome())
+			.append(";")
+			.append(node.getNota())
+			.append(")\n");
+			node = node.getNext();
+		}
+		sb.append("null.");
+		
+		return sb.toString();
+	}
+
+}
---
+++ Apl2_arquivosNecessarios/src/apl2/Node.java
@@ -0,0 +1,72 @@
+// arquivo: src/apl2/Node.java
+
+// TODO: Colocar a identificação dos(as) integrantes aqui.
+
+package apl2;
+
+// -- A classe Node (que pertence ao pacote apl2) deve conter os atributos que
+// representam a nova versão dos dados de uma pessoa, conforme descrito no
+// enunciado da atividade Apl2.
+// -- A classe deve conter os construtores apropriados, assim como os métodos
+// getters e setters.
+// -- A classe também representa um nó que é usado na implementação da lista
+// duplamente encadeada (classe DLinkedList).
+// -- A classe deve sobrescrever (override) o método public String toString()
+// {...}, retornando uma string com os valores dos atributos da classe.
+
+public class Node {
+	
+	private String id;
+	private String nome;
+	private float nota;
+	private Node next;
+	
+	public Node() {
+		this("", "", 0, null);
+	}
+	
+	public Node(String id, String nome, float nota, Node next) {
+		this.id = id;
+		this.nome = nome;
+		this.nota = nota;
+		this.next = next;
+	}
+	
+	public String getId() {
+		return id;
+	}
+	
+	public void setId(String id) {
+		this.id = id;
+	}
+	
+	public String getNome() {
+		return nome;
+	}
+	
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	
+	public float getNota() {
+		return nota;
+	}
+	
+	public void setNota(float nota) {
+		this.nota = nota;
+	}
+
+	public Node getNext() {
+		return next;
+	}
+	
+	public void setNext(Node next) {
+		this.next = next;
+	}
+	
+	@Override
+	public String toString() {
+		return "[dados: (" + id + ";" + nome + ";" + nota + ") | next: " + next + "]";
+	}
+
+}
